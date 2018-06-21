5ce27d8afbd2a9dfa391d151bc0e24fd44f12700
peterokwara <peter.okwara@outlook.com>
2018-06-21 23:37:42 +0300

    Added some nlp to the code

--- app.js
+++ app.js
@@ -2,31 +2,32 @@
 const TelegramBot = require('node-telegram-bot-api');
 const config = require('config');
 const request = require("request");
+const fetch = require('node-fetch');
+
+// Fetch wit.ai token from the config file
+const NEW_ACCESS_TOKEN = config.get('witAiToken');
 
 // Fetch the telegram token from the config file
 const telegramToken = config.get('telegramToken');
+
+// Set url to fetch from the now environment
 const url = process.env.NOW_URL;
 
-// const bot = new TelegramBot(telegramToken, { polling: true });
-
+// Set up the webhook and the bot
 const options = { webHook: { port: 443 } };
-
 const bot = new TelegramBot(telegramToken, options);
-
 bot.setWebHook(`${url}/bot${telegramToken}`);
 
-
+// For any incoming message
 bot.on('message', (msg) => {
 
-    if (msg.text.toString().toLowerCase().length == 12) {
-        var accountName = msg.text.toString().toLowerCase();
-        let account_balance = checkAccountName(accountName, msg);
-
-    }
+    // For any incoming message, send it to the nlp
+    handleMessage(msg);
 
 });
 
-function checkAccountName(accountName, msg) {
+// Checks account balance in the eos api
+function checkAccountName(accountName, id) {
     var options = {
         method: "POST",
         url: "http://mainnet.eoscanada.com/v1/chain/get_account",
@@ -41,8 +42,69 @@ function checkAccountName(accountName, msg) {
         if (error) throw new Error(error);
         let accountBalance = body.core_liquid_balance;
         console.log(body.core_liquid_balance);
-        bot.sendMessage(msg.chat.id, accountBalance);
+
+        // Send account balance to user
+        bot.sendMessage(id, `The account balance is `+ accountBalance);
 
     });
+}
+
+// Get the context
+function handleMessage(message_in) {
+    let message = message_in.text;
+    let id = message_in.chat.id;
+    return queryWit(message).then(({ entities }) => {
+        const intent = firstEntity(entities, 'intent');
+        console.log(intent);
+
+        if (!intent) {
+            bot.sendMessage(id, 'Sorry, try again');
+            return;
+        }
+
+        switch (intent.value) {
+
+            // if it is asking account balance
+            case 'balance':
+
+                // The word with 12 characters is the account name
+                var strSplit = message.split(" ");
+                console.log(strSplit);
+
+                for (let i = 0; i < strSplit.length; i++) {
+                    if (strSplit[i].length == 12) {
+                        let accountName = strSplit[i];
+                        console.log(accountName);
+                        checkAccountName(accountName, id);
+
+                    }
+                }
+
+
+        }
+    });
+
+}
+
+// Wit ai specific code
+function firstEntity(entities, name) {
+    return entities &&
+        entities[name] &&
+        Array.isArray(entities[name]) &&
+        entities[name] &&
+        entities[name][0];
+}
+
+// Wit ai specific code
+function queryWit(text, n = 1) {
+    return fetch(
+        `https://api.wit.ai/message?v=20170307&n=${n}&q=${encodeURIComponent(text)}`,
+        {
+            headers: {
+                Authorization: `Bearer ${NEW_ACCESS_TOKEN}`,
+                'Content-Type': 'application/json',
+            },
+        }
+    ).then(res => res.json());
 }
 

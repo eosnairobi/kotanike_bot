4bfe46db8f53a43b49f5068d2b38e03add4938d2
p.okwara <peter.okwara@outlook.com>
2018-07-07 02:11:26 +0300

    Get relevant information about a block producer

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const TelegramBot = require('node-telegram-bot-api');
 const config = require('config');
 const request = require("request");
 const fetch = require('node-fetch');
+var _ = require("lodash");
 
 // Fetch wit.ai token from the config file
 const NEW_ACCESS_TOKEN = config.get('witAiToken');
@@ -28,14 +29,14 @@ bot.on('message', (msg) => {
 
 // Message to send on start
 bot.onText(/\/start/, (msg) => {
-    bot.sendMessage(msg.chat.id, "Hi and welcome to Kotani by @eosnairobi block producer candidate").then(() => bot.sendMessage(msg.chat.id, "Some of the questions you can ask are: \n - balance ha2tsmzqhege \n - eos price")).then(() => bot.sendMessage(msg.chat.id, "In case you get stuck, you can try /help to show you commands you can try"));
+    bot.sendMessage(msg.chat.id, "Hi and welcome to Kotani by @eosnairobi block producer candidate").then(() => bot.sendMessage(msg.chat.id, "Some of the questions you can ask are: \n - balance ha2tsmzqhege \n - eos price \n - get producer accountname")).then(() => bot.sendMessage(msg.chat.id, "In case you get stuck, you can try /help to show you commands you can try"));
 });
 
 // Message to send on start
 bot.onText(/\/help/, (msg) => {
 
-    bot.sendMessage(msg.chat.id, "Some of the questions you can ask are: \n - balance accountname \n - eos price" );
-        
+    bot.sendMessage(msg.chat.id, "Some of the questions you can ask are: \n - balance accountname \n - eos price \n - get producer accountname");
+
 })
 
 // Checks account balance in the eos api
@@ -60,7 +61,7 @@ function checkAccountBalance(accountName, id) {
             bot.sendMessage(id, `The account balance is ` + accountBalance);
         } else {
             bot.sendMessage(id, `The account name you entered is invalid or command entered is incorrect.`)
-            .then(() => bot.sendMessage(id, `Try: \n - balance accountname`));
+                .then(() => bot.sendMessage(id, `Try: \n - balance accountname`));
         }
 
     });
@@ -75,7 +76,7 @@ function handleMessage(message_in) {
         console.log(intent);
 
         if (!intent && message.text != '/help' && message.text != '/start') {
-            bot.sendMessage(id, 'Hi, some of the questions you can ask are: \n - balance accountname \n - eos price');
+            bot.sendMessage(id, 'Hi, some of the questions you can ask are: \n - balance accountname \n - eos price \n - get producer accountname');
             return;
         }
 
@@ -89,8 +90,17 @@ function handleMessage(message_in) {
                 checkAccountBalance(accountName, id);
                 break;
 
+            // if user is asking for the price of eos
             case 'price':
                 getPrice(id);
+                break;
+
+            // if user is asking for information about a block producer candidate
+            case 'producer':
+                // The last word in the sentence is the producer name
+                var sentence_one = message.split(" ");
+                var producerName = sentence_one[sentence_one.length - 1];
+                getProducerInfo(producerName, id);
                 break;
         }
     });
@@ -122,14 +132,39 @@ function queryWit(text, n = 1) {
 // Gets prices from coinmarketcap
 function getPrice(id) {
 
-    request('https://api.coinmarketcap.com/v1/ticker/EOS/', function(error, response, body){
+    request('https://api.coinmarketcap.com/v1/ticker/EOS/', function (error, response, body) {
         var data = JSON.parse(body);
         var price = data[0]["price_usd"];
         bot.sendMessage(id, '1 EOS token is equivalent to ' + price + ' US Dollars');
     });
 }
 
-// Gets all the block producer candidates
-function getProducer(){
+// Checks account balance in the eos api
+function getProducerInfo(accountName, id) {
 
+    var options = {
+        method: 'POST',
+        url: 'http://mainnet.eoscanada.com/v1/chain/get_producers',
+        body: { limit: '500', json: 'true' },
+        json: true
+    };
+
+    request(options, function (error, response, body) {
+        if (error) throw new Error(error);
+        var producers = body.rows;
+        var producer_info = _.filter(producers, { owner: accountName });
+        if (producer_info != '') {
+            var producer_name = producer_info[0].owner;
+            var url = producer_info[0].url;
+            var rank = _.findIndex(producers, { owner: accountName }) + 1;
+            var total_votes = producer_info[0].total_votes;
+            var overal_total_votes = body.total_producer_vote_weight;
+            var vote_percentage = (total_votes / overal_total_votes) * 100;
+
+            bot.sendMessage(id, `About ` + producer_name + `\n\nUrl: `+ url + `\n\nRank: ` + rank + `\n\nTotal votes: `+ total_votes + `\n\nOveral number of votes: ` + overal_total_votes + `\n\nVote percentage: ` + vote_percentage);
+        } else {
+            bot.sendMessage(id, `The account name you entered is invalid or command entered is incorrect.`)
+                .then(() => bot.sendMessage(id, `Try: \n - get producer accountname`));
+        }
+    });
 }
